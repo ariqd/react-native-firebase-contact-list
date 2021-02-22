436cd451684c02ccf9cbd4f79ea869deee9b443b
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-02-22 08:15:11 +0700

    Initial Commit

---
+++ src/config/Firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const FIREBASE = firebase.initializeApp({
+  apiKey: 'AIzaSyAaBKdimOOCWZwGb1OJyDpLSZZYv525eRI',
+  authDomain: 'crud-react-native-29c24.firebaseapp.com',
+  projectId: 'crud-react-native-29c24',
+  storageBucket: 'crud-react-native-29c24.appspot.com',
+  messagingSenderId: '247748832632',
+  appId: '1:247748832632:web:873ede5f1165fa036a6c8d',
+});
+
+export default FIREBASE;
---
+++ src/pages/Home.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Text, StyleSheet, View, TouchableOpacity} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faPlus} from '@fortawesome/free-solid-svg-icons';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text> Halaman Home </Text>
+      <View style={styles.buttonWrapper}>
+        <TouchableOpacity style={styles.buttonAdd}>
+          <FontAwesomeIcon icon={faPlus} size={25} color="white" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonWrapper: {
+    flex: 1,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    margin: 30,
+  },
+  buttonAdd: {
+    padding: 20,
+    backgroundColor: 'skyblue',
+    borderRadius: 30,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
+
+export default Home;
